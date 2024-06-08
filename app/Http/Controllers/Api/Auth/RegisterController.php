b28c7086a505c4e6b188cc831ca84a9497b238a8
Md Akbar Hossain <akbarhossain160@gmail.com>
2024-06-08 20:13:55 +0600

    First Commit

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+        ], 201);
+
+    }
+}
